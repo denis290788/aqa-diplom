6a30bb325963ae7a54d6ffe735f8151e7fe1f7d4
denis290788 <denis290788@gmail.com>
2020-11-18 21:42:31 +0300

    commit 2

---
+++ src/test/java/dbUtils/OrderEntity.java
@@ -0,0 +1,11 @@
+package dbUtils;
+
+import lombok.Data;
+
+@Data
+public class OrderEntity {
+    private String id;
+    private String created;
+    private String credit_id;
+    private String payment_id;
+}
