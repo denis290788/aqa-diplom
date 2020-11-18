98e30ccc055514aa3f0ff2933252d037ea76e764
denis290788 <denis290788@gmail.com>
2020-11-18 21:46:16 +0300

    commit 2

--- src/test/java/pages/CreditPage.java
+++ src/test/java/pages/CreditPage.java
@@ -5,8 +5,8 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$$;
 
-public class CreditPage {
-    private SelenideElement heading = $$(".heading").find(Condition.exactText("Оплата по карте"));
+public class CreditPage extends OfferPage {
+    private SelenideElement heading = $$(".heading").find(Condition.exactText("Кредит по данным карты"));
 
     public CreditPage() {
         heading.shouldBe(Condition.visible);
