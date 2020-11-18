4397ad6a753cac7eac0c2e9a1d7ec1b957692ef0
denis290788 <denis290788@gmail.com>
2020-11-18 21:42:47 +0300

    commit 2

---
+++ src/test/java/dbUtils/PaymentEntity.java
@@ -0,0 +1,12 @@
+package dbUtils;
+
+import lombok.Data;
+
+@Data
+public class PaymentEntity {
+    private String id;
+    private String amount;
+    private String created;
+    private String status;
+    private String transaction_id;
+}
