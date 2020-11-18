098064b707617964d268d4005e5cd6526207fc5f
denis290788 <denis290788@gmail.com>
2020-11-18 21:46:03 +0300

    commit 2

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -3,11 +3,28 @@ package data;
 import com.github.javafaker.Faker;
 import lombok.Value;
 
-import java.time.LocalDate;
 import java.util.Random;
 
 public class DataHelper {
     private DataHelper() {
+    }
+
+    @Value
+    public static class CardNumber {
+        private String cardNumber;
+
+        public static CardNumber getApprovedCardNumber() {
+            return new CardNumber("4444 4444 4444 4441");
+        }
+        public static CardNumber getDeclinedCardNumber() {
+            return new CardNumber("4444 4444 4444 4442");
+        }
+        public static CardNumber getNonexistentCardNumber() {
+            return new CardNumber("4444 4444 4444 4443");
+        }
+        public static CardNumber getIlnvalidCardNumber() {
+            return new CardNumber("WWWW dii9 #%#%  889");
+        }
     }
 
     @Value
@@ -18,6 +35,48 @@ public class DataHelper {
             Faker faker = new Faker();
             String ownerName = faker.name().fullName();
             return new OwnerName(ownerName);
+        }
+
+        public static OwnerName getInvalidOwnerName() {
+            return new OwnerName("111 &fkdjhf");
+        }
+    }
+
+    @Value
+    public static class CardMonth {
+        private String cardMonth;
+
+        public static CardMonth getRandomCardMonth() {
+            Random random = new Random();
+            int month = 1 + random.nextInt(12);
+            return new CardMonth(String.format("%02d", month));
+        }
+
+        public static CardMonth getWrongCardMonth() {
+            return new CardMonth("13");
+        }
+
+        public static CardMonth getInvalidCardMonth() {
+            return new CardMonth("&&");
+        }
+    }
+
+    @Value
+    public static class CardYear {
+        private String cardYear;
+
+        public static CardYear getValidCardYear() {
+            Faker faker = new Faker();
+            int year = faker.number().numberBetween(21,24);
+            return new CardYear(Integer.toString(year));
+        }
+
+        public static CardYear getEarlyCardYear() {
+            return new CardYear("19");
+        }
+
+        public static CardYear getWrongCardYear() {
+            return new CardYear("FF");
         }
     }
 
@@ -31,45 +90,9 @@ public class DataHelper {
             String cardCvv = Integer.toString(intCardCvv);
             return new CardCvv(cardCvv);
         }
-    }
 
-    @Value
-    public static class CardNumber {
-        private String cardNumber;
-
-        public static CardNumber getApprovedCardNumber() {
-            return new CardNumber("4444 4444 4444 4441");
-        }
-        public static CardNumber getDeclinedCardNumber() {
-            return new CardNumber("4444 4444 4444 4442");
-        }
-    }
-
-    @Value
-    public static class CardMonth {
-        private String cardMonth;
-
-        public static CardMonth getRandomCardMonth() {
-            Random random = new Random();
-            int month = 1 + random.nextInt(12);
-            return new CardMonth(String.format("%02d", month));
-        }
-    }
-
-    @Value
-    public static class CardYear {
-        private String cardYear;
-
-        public static CardYear getCurrentCardYear() {
-            LocalDate currentDate = LocalDate.now();
-            int year = currentDate.getYear() - 2000;
-            return new CardYear(Integer.toString(year));
-        }
-
-        public static CardYear getValidCardYear() {
-            Faker faker = new Faker();
-            int year = faker.number().numberBetween(21,24);
-            return new CardYear(Integer.toString(year));
+        public static CardCvv getInvalidCardCvv() {
+            return new CardCvv("Y7$");
         }
     }
 }
