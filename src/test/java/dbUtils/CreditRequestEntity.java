d8eac55547378d62d4495bb69b9aed7c8e440779
denis290788 <denis290788@gmail.com>
2020-11-18 21:41:46 +0300

    commit 2

---
+++ src/test/java/dbUtils/CreditRequestEntity.java
@@ -0,0 +1,11 @@
+package dbUtils;
+
+import lombok.Data;
+
+@Data
+public class CreditRequestEntity {
+    private String id;
+    private String bank_id;
+    private String created;
+    private String status;
+}
